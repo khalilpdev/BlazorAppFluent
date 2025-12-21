4a4b739f31987ad95262bf6a435ea5f12a8d9c24
Leandro Khalil <khalilpdev@gmail.com>
2025-12-21 15:06:19 -0300

    refactor and created Shared Project

---
+++ BlazorAppFluent.Shared/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorAppFluent.Shared.Models
+{
+    internal class Tarefa
+    {
+    }
+}
---
+++ BlazorAppFluent.Shared/Models/WeatherForecast.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorAppFluent.Shared.Models;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public static IQueryable<WeatherForecast> GetFakeData()
+    {
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).AsQueryable();
+
+        return forecasts;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date}: {Summary}, {TemperatureC}C/{TemperatureF}F";
+    }
+}
